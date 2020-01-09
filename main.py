72bc7a37c7cce7e63f8c6908b776b7c21c379133
markus-flicke <mark.alex@web.de>
2020-01-09 20:22:55 +0100

    commandline_added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import PyPDF2 as pdf
+import os
+import argparse
+from PyPDF2 import PdfFileReader
 
 
-def read_pdf(filename='10_detection_tracking.pdf'):
+def read_pdf(filename):
+    """
+    Reads all video links from a PDF. Only tested for slidedeck PDFs created with PDFLatex.
+    :param filename:
+    :return:
+    """
     with open(filename, 'rb') as f:
-        reader = pdf.PdfFileReader(f)
+        reader = PdfFileReader(f)
         paths = []
         for i in range(reader.getNumPages()):
             path = read_page(i, reader)
@@ -13,6 +20,12 @@ def read_pdf(filename='10_detection_tracking.pdf'):
 
 
 def read_page(n, reader):
+    """
+    Given a reader object, reads the video paths from the n'th page
+    :param n:
+    :param reader:
+    :return:
+    """
     pageObj = reader.getPage(n)
     if not '/Annots' in pageObj.keys(): return
     paths = []
@@ -23,6 +36,33 @@ def read_page(n, reader):
     return paths
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='Filename', action='store', nargs='*')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    for video_path in read_pdf():
-        print(video_path)
+    """
+    User can specify either a pdf, 
+    or a path to be read from, 
+    or nothing in which case I will process the pdfs in the script's directory. 
+    
+    All videos in the pdf(s) are printed.
+    (verified results on lecture 9 and 10)
+    """
+    if not parse_args().path:
+        path = "."
+    else:
+        path = parse_args().path[0]
+
+    if path.endswith('.pdf'):
+        for video_path in read_pdf(path):
+            print(video_path)
+    else:
+        files = os.listdir(path)
+        for file in files:
+            if file.endswith('.pdf'):
+                pdf_path = os.path.join(path, file)
+                for video_path in read_pdf(pdf_path):
+                    print(video_path)
